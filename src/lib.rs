15d619b229ee6157e2df918fc3f82470eb53af62
Pierre Chifflier <chifflier@wzdftpd.net>
2017-10-13 10:15:00 +0200

    Add crate description, add more QA checks

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+#![deny(// missing_docs,
+        missing_debug_implementations,
+        unsafe_code,
+        unstable_features,
+        unused_import_braces, unused_qualifications)]
+
 #[macro_use] extern crate nom;
 #[macro_use] extern crate rusticata_macros;
 
